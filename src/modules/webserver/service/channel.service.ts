4687cd64fc1159b3a0baf5ecab06c946889b517f
VeguiDev <veguigameplays@gmail.com>
2023-02-08 18:18:28 -0300

    ChannelService for the ChannelController

---
+++ src/modules/webserver/service/channel.service.ts
@@ -0,0 +1,49 @@
+import {GetChannelReward, GetChannelRewards} from '../../../api/users.service';
+import AuthStore from '../../../class/AuthStore.class';
+
+export default class ChannelService {
+
+    async getChannelRewards() {
+
+        const credentials = await AuthStore.getInstance().getCredentials();
+
+        if(!credentials) {
+            throw "not_credentials";
+        }
+
+        const {error, data, res} = await GetChannelRewards(credentials.user.user_id);
+
+        if(!error) {
+
+            return data.data;
+
+        }
+        
+        console.error(error);
+        return null;
+
+    }
+
+    async getChannelReward(id:string) {
+
+        const credentials = await AuthStore.getInstance().getCredentials();
+
+        if(!credentials) {
+            throw "not_credentials";
+        }
+
+        const {error, data, res} = await GetChannelReward(credentials.user.user_id, id);
+
+        if(!error) {
+
+            return data.data;
+
+        }
+        
+        console.error(error);
+        return null;
+
+    }
+
+
+}
