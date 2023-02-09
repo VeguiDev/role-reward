fe2b47abaca844fdaa252ffcfe8b43ea2f8b1b7c
VeguiDev <veguigameplays@gmail.com>
2023-02-09 01:28:31 -0300

    Twitch Module Improt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { ModulesConfig } from "./lib/modules.lib";
 import { WebServer } from "./modules/webserver/WebServer.class";
 import dotenv from 'dotenv';
+import DiscordModule from "./modules/discord/discord.class";
+import ModulesConfigC from "./class/ModulesConfig.class";
+import TwitchModule from "./modules/twitch/Twitch.class";
 
 dotenv.config();
 
@@ -9,5 +12,19 @@ if(ModulesConfig.webserver.enabled) {
     const webserver = new WebServer();
 
     webserver.listen();
+
+}
+
+if(ModulesConfig.discord.enabled) {
+    const discordModule = DiscordModule.getInstance();
+    
+    discordModule.start();
+}
+
+if(ModulesConfig.twitch.enabled) {
+
+    const twitchModule = TwitchModule.getInstance();
+
+    twitchModule.listen();
 
 }
