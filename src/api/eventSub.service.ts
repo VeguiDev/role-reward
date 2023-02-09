53cc6edcd253726fe217b9f3e7051565fee76d15
VeguiDev <veguigameplays@gmail.com>
2023-02-09 01:28:45 -0300

    EventSub Service

---
+++ src/api/eventSub.service.ts
@@ -0,0 +1,24 @@
+import { HelixApiClient as apiClient } from "./helixApiClient";
+import { ResponseWrapper } from "./lib/ResponseWreapper.lib";
+
+export async function EventSubSubscribe(event:string, session_id:string) {
+
+    return await ResponseWrapper(
+        apiClient({
+            url: "eventsub/subscriptions",
+            method: "POST",
+            data: {
+                type: event,
+                version: 1,
+                condition: {
+                    broadcaster_user_id: "::USER_ID::"
+                },
+                transport: {
+                    method: "websocket",
+                    session_id: session_id
+                }
+            }
+        })
+    )
+
+}
