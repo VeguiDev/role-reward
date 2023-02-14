346f75e5ec1e79bfbe25647938ec11d7a2ca717c
VeguiDev <veguigameplays@gmail.com>
2023-02-14 20:37:17 -0300

    Cli Module
    Comamnds:
    - status [module]

--- src/modules/cli/cli.class.ts
+++ src/modules/cli/cli.class.ts
@@ -1,21 +1,61 @@
-export default class CLIModule {
+import inquirer from 'inquirer';
+import { StatusCommand } from './commands/status.command';
+import chalk from 'chalk';
 
-    private static instance:CLIModule;
+export default class CLIModule {
+    private static instance: CLIModule;
+
+    private loop: boolean = true;
+
+    setLoop(value: boolean) {
+        this.loop = value;
+    }
+
+    isInLoop() {
+        return this.loop;
+    }
+
+    private async command(command: string) {
+        command = command.trim();
+
+        if (command.startsWith('status')) {
+            StatusCommand(command);
+        } else if(command.length > 0) {
+            let fristPart = command.split(' ')[0];
+
+            console.log(
+                chalk.gray(fristPart),
+                chalk.redBright('Command not found!')
+            );
+        }
+
+        this.prompt();
+    }
+
+    async prompt() {
+        console.log();
+
+        let ans = await inquirer.prompt({
+            type: 'input',
+            name: 'command',
+            message: chalk.green('>'),
+            prefix: '',
+        });
+
+        console.log();
+
+        await this.command(ans.command);
+    }
 
     start() {
-        
+        this.prompt();
     }
 
     static getInstance() {
-
-        if(!this.instance) {
-
+        if (!this.instance) {
             this.instance = new CLIModule();
-
         }
 
         return this.instance;
-
     }
-
 }
