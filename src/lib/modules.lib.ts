cfc5a9b7d34e0fd4220ebb8cd4d328726a7150e3
VeguiDev <veguigameplays@gmail.com>
2023-02-13 23:20:57 -0300

    modules lib deleted

--- src/lib/modules.lib.ts
+++
@@ -1,6 +0,0 @@
-import yaml from 'yaml';
-import path from 'path';
-import fs from 'fs';
-import ModulesConfigC from '../class/ModulesConfig.class';
-
-export const ModulesConfig = ModulesConfigC.getInstance().getData();
