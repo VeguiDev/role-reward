c9646d03a990866bec2e4c6923e6b047aa6867ec
VeguiDev <veguigameplays@gmail.com>
2023-02-16 06:05:18 -0300

    Command: Disconnect close

--- src/modules/cli/cli.class.ts
+++ src/modules/cli/cli.class.ts
@@ -2,19 +2,11 @@ import inquirer from 'inquirer';
 import { StatusCommand } from './commands/status.command';
 import chalk from 'chalk';
 import ActionCommand from './commands/action.comand';
+import DisconnectCommand from './commands/disconnect.command';
+import Application from '../../class/Application.class';
 
 export default class CLIModule {
     private static instance: CLIModule;
-
-    private loop: boolean = true;
-
-    setLoop(value: boolean) {
-        this.loop = value;
-    }
-
-    isInLoop() {
-        return this.loop;
-    }
 
     private async command(command: string) {
         console.log("RAW COMMAND:", command);
@@ -28,6 +20,29 @@ export default class CLIModule {
             await StatusCommand(command);
         } else if(cmdName == "action" || cmdName == "actions") {
             await new ActionCommand().cmd(command);
+        } else if(cmdName == "disconnect") {
+            await new DisconnectCommand().cmd(command);
+        } else if(cmdName == "close" || cmdName == "exit") {
+
+            const {close} =await inquirer.prompt([
+                {
+                    type: "confirm",
+                    message: "Are you sure you want to exit?",
+                    name: "close"
+                }
+            ])
+
+            if(close) {
+                await Application.getInstance().stop()
+                await Application.getInstance().saveAll();
+                console.log(chalk.greenBright("Goodbye :)"));
+                process.exit(0);
+            }
+
+        } else if(cmdName == "save-all") {
+            await Application.getInstance().saveAll();
+        } else if(cmdName == "reconnect") {
+            await Application.getInstance().reconnect();
         } else if(command.length > 0) {
             let fristPart = command.split(' ')[0];
 
