b6da8c93ebd50da5e280462f3aababee30e3e4fb
VeguiDev <veguigameplays@gmail.com>
2023-02-20 03:38:50 -0300

    Cannon user interface

--- src/interfaces/TwitchUser.interface.ts
+++ src/interfaces/TwitchUser.interface.ts
@@ -4,3 +4,19 @@ export default interface TwitchUserI {
     user_id:number;
 
 }
+
+export interface CannonTwitchUserI {
+    name?:string;
+    user_id?:number;
+    id:number;
+    login:string;
+    display_name:string;
+    type:("admin"|"global_mod"|"staff"|string);
+    broadcaster_type:("affiliate"|"parther"|string);
+    description:string;
+    profile_image_url:string;
+    offline_image_url:string;
+    view_count:number;
+    email:string;
+    created_at:string;
+}
